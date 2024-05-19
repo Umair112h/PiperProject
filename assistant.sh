80da7180b55fa3ded43cbf9b80960acfa817b0d2
Umair <umairrafiq156@gmail.com>
2024-05-20 00:58:16 +0530

    removed the frequent 'asterisks' from the output

--- assistant.sh
+++ assistant.sh
@@ -4,4 +4,4 @@ echo "What can i do for you"
 
 read text
 
-tgpt -q "($text)" | ./piper/piper --model piper/en_GB-cori-high.onnx --output-raw | aplay -r 22050 -f S16_LE -t raw -
+tgpt -q "($text)" | tr -d '*' | ./piper/piper --model piper/en_GB-cori-high.onnx --output-raw | aplay -r 22050 -f S16_LE -t raw -
